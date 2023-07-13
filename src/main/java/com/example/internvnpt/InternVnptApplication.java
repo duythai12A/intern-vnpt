f80b27d00f010f6059646c640d412d2df9b6df84
duythai12A <duythai10a@gmail.com>
2023-07-13 09:52:34 +0700

    create project

---
+++ src/main/java/com/example/internvnpt/InternVnptApplication.java
@@ -0,0 +1,13 @@
+package com.example.internvnpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternVnptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternVnptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internvnpt/InternVnptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internvnpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternVnptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
